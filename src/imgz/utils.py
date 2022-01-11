7e8d6faec60f2e827e9cc6dbfe5d3dc393852417
cvdnn <cvvdnn@gmail.com>
2022-01-11 19:42:55 +0800

    INIT

--- src/imgz/utils.py
+++ src/imgz/utils.py
@@ -1,12 +1,10 @@
 # -*- coding: utf8 -*-
 
-from xlang import iox
+from pathlib import Path
 
 # 图片后缀名
 SUFFIX_IMAGE = ['.jpg', '.png', '.jpeg', '.bmp']
 
 
 def is_image_file(file_path):
-    _, _, suffix = iox.split_path(file_path)
-
-    return suffix.lower() in SUFFIX_IMAGE
+    return Path(file_path).suffix.lower() in SUFFIX_IMAGE
