ec44edc4228fca2f37e1753786b3aa1d398288f4
cvdnn <cvvdnn@gmail.com>
2022-01-25 19:28:31 +0800

    INIT

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="imgz",
-    version="0.0.8",
+    version="0.0.9",
 
     author="cvdnn",
     author_email="cvvdnn@gmail.com",
