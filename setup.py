1c958e5f7118660f345fce2d972ca2b0f1da7468
cvdnn <cvvdnn@gmail.com>
2022-01-25 19:35:10 +0800

    INIT

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="imgz",
-    version="0.0.9",
+    version="0.0.10",
 
     author="cvdnn",
     author_email="cvvdnn@gmail.com",
--- src/imgz/transform.py
+++ src/imgz/transform.py
@@ -23,10 +23,10 @@ def normalizing(img, dtype=D_TYPE):
     return img.astype(dtype) / 255
 
 
-def smooth(img, mask, anchor=(5, 5)):
-    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, anchor=anchor)
+def smooth(img, mask, ksize=(5, 5)):
+    kernel = cv2.getStructuringElement(cv2.MORPH_RECT, ksize=ksize)
     gt_mask = cv2.dilate(mask, kernel, iterations=1)
-    blur = cv2.blur(img, anchor=anchor)
+    blur = cv2.blur(img, ksize=ksize)
     output = blend(blur, gt_mask, bg=img, morph=True)
 
     output = blend(img, mask, bg=output, morph=True)
